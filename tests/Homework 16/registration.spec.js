bd6f21810c37cbd943fa8a1438c51ed8a04a6ac1
olenanikitina1321 <o.nikitina@geniusee.com>
2023-12-22 01:09:59 +0100

    Homework 16.1

--- tests/Homework 16/registration.spec.js
+++ tests/Homework 16/registration.spec.js
@@ -13,7 +13,6 @@ test.describe.only('Registration', async () => {
         await page.goto('/')
     })
 
-
     test('Registration with correct data', async () => {
         const loginBtn = page.locator('button', {
             hasText: 'Sign in'
@@ -56,7 +55,8 @@ test.describe.only('Registration', async () => {
 
 
 });
-    test('Name validation', async () => {
+
+ test('Name validation - number of symbols', async () => {
 
         const loginBtn = page.locator('button', {
             hasText: 'Sign in'
@@ -76,29 +76,75 @@ test.describe.only('Registration', async () => {
         await expect(error).toHaveText('Name has to be from 2 to 20 characters long')
         await inputName.clear()
 
+    });
+
+    test('Name validation - required field', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click()
+        await registrationBtn.click();
+
+        const inputName = page.locator('#signupName')
+        const error = page.locator('div.invalid-feedback')
+
         await inputName.fill('')
         await inputName.blur()
         await expect(error).toHaveText('Name required')
         await inputName.clear()
+
+    });
+
+    test('Name validation - invalid data', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const inputName = page.locator('#signupName')
+        const error = page.locator('div.invalid-feedback')
 
         await inputName.fill('111')
         await inputName.blur()
         await expect(error).toHaveText('Name is invalid')
         await inputName.clear()
 
+    });
+
+    test('Name validation - border colour', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const inputName = page.locator('#signupName')
+
         await inputName.fill('111')
         await inputName.blur()
-        await page.waitForTimeout(100);
-        const borderColor = await inputName.evaluate((element) => {
-            return window.getComputedStyle(element).borderColor;
-        })
-        await expect(borderColor).toBe('rgb(220, 53, 69)')
+        await expect(inputName).toHaveCSS('border-color', 'rgb(220, 53, 69)')
         await inputName.clear()
 
 
     });
 
-    test('Last Name validation', async () => {
+    test('Last Name validation - number of symbols', async () => {
 
         const loginBtn = page.locator('button', {
             hasText: 'Sign in'
@@ -118,29 +164,72 @@ test.describe.only('Registration', async () => {
         await expect(error).toHaveText('Last name has to be from 2 to 20 characters long')
         await inputLastName.clear()
 
+    });
+
+    test('Last Name validation - required field', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const inputLastName = page.locator('#signupLastName')
+        const error = page.locator('div.invalid-feedback')
+
         await inputLastName.fill('')
         await inputLastName.blur()
         await expect(error).toHaveText('Last name required')
         await inputLastName.clear()
+    });
+
+    test('Last Name validation - invalid data', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const inputLastName = page.locator('#signupLastName')
+        const error = page.locator('div.invalid-feedback')
 
         await inputLastName.fill('111')
         await inputLastName.blur()
         await expect(error).toHaveText('Last name is invalid')
         await inputLastName.clear()
+    });
+
+    test('Last Name validation - border colour', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const inputLastName = page.locator('#signupLastName')
+
 
         await inputLastName.fill('111')
         await inputLastName.blur()
-        await page.waitForTimeout(100);
-        const borderColor = await inputLastName.evaluate((element) => {
-            return window.getComputedStyle(element).borderColor;
-        })
-        await expect(borderColor).toBe('rgb(220, 53, 69)')
+        await expect(inputLastName).toHaveCSS('border-color', 'rgb(220, 53, 69)');
         await inputLastName.clear()
-
-
     });
 
-    test('Email validation', async () => {
+    test('Email validation - required field', async () => {
 
         const loginBtn = page.locator('button', {
             hasText: 'Sign in'
@@ -159,24 +248,52 @@ test.describe.only('Registration', async () => {
         await email.blur()
         await expect(error).toHaveText('Email required')
         await email.clear()
+    });
+
+    test('Email validation - invalid data', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const email = page.locator('#signupEmail')
+        const error = page.locator('div.invalid-feedback')
 
         await email.fill('111')
         await email.blur()
         await expect(error).toHaveText('Email is incorrect')
         await email.clear()
 
+    });
+
+    test('Email validation - border colour', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const email = page.locator('#signupEmail')
+
         await email.fill('111')
         await email.blur()
-        await page.waitForTimeout(100);
-        const borderColor = await email.evaluate((element) => {
-            return window.getComputedStyle(element).borderColor;
-        })
-        await expect(borderColor).toBe('rgb(220, 53, 69)')
+        await expect(email).toHaveCSS('border-color', 'rgb(220, 53, 69)')
         await email.clear()
 
-
     });
-    test('Password validation', async () => {
+
+    test('Password validation - required field', async () => {
 
         const loginBtn = page.locator('button', {
             hasText: 'Sign in'
@@ -197,25 +314,95 @@ test.describe.only('Registration', async () => {
         await password.clear()
 
 
+    });
+
+    test('Password validation - data validation', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const password = page.locator('#signupPassword')
+        const error = page.locator('div.invalid-feedback')
+
         await password.fill('111')
         await password.blur()
         await expect(error).toHaveText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')
         await password.clear()
 
+});
+
+    test('Password validation - border colour', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const password = page.locator('#signupPassword')
 
         await password.fill('111')
         await password.blur()
-        await page.waitForTimeout(100);
-        const borderColor1 = await password.evaluate((element) => {
-            return window.getComputedStyle(element).borderColor;
-        })
-        await expect(borderColor1).toBe('rgb(220, 53, 69)')
+        await expect(password).toHaveCSS('border-color', 'rgb(220, 53, 69)')
         await password.clear()
 
+    });
+    test('Password confirmation validation - required field', async () => {
 
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const passwordConfirmation = page.locator('#signupRepeatPassword')
+        const error = page.locator('div.invalid-feedback')
+
+
+        await passwordConfirmation.fill('')
+        await passwordConfirmation.blur()
+        await expect(error).toHaveText('Re-enter password required')
+        await passwordConfirmation.clear()
 
     });
-    test('Password confirmation validation', async () => {
+
+    test('Password confirmation validation - invalid data', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const passwordConfirmation = page.locator('#signupRepeatPassword')
+        const error = page.locator('div.invalid-feedback')
+
+        await passwordConfirmation.fill('111')
+        await passwordConfirmation.blur()
+        await expect(error).toHaveText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')
+        await passwordConfirmation.clear()
+
+    });
+    test('Password confirmation validation - different passwords ', async () => {
 
         const loginBtn = page.locator('button', {
             hasText: 'Sign in'
@@ -231,38 +418,36 @@ test.describe.only('Registration', async () => {
         const passwordConfirmation = page.locator('#signupRepeatPassword')
         const error = page.locator('div.invalid-feedback')
 
-
-        await passwordConfirmation.fill('')
-        await passwordConfirmation.blur()
-        await expect(error).toHaveText('Re-enter password required')
-        await passwordConfirmation.clear()
-
-
-        await passwordConfirmation.fill('111')
-        await passwordConfirmation.blur()
-        await expect(error).toHaveText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter')
-        await passwordConfirmation.clear()
-
-
         await password.fill('1321@Helena13')
         await passwordConfirmation.fill('1321@Helena')
         await passwordConfirmation.blur()
         await expect(error).toHaveText('Passwords do not match')
         await passwordConfirmation.clear()
         await password.clear()
+    });
+
+    test('Password confirmation validation - border colour', async () => {
+
+        const loginBtn = page.locator('button', {
+            hasText: 'Sign in'
+        })
+        const registrationBtn = page.locator('button', {
+            hasText: 'Registration'
+        })
+
+        await loginBtn.click();
+        await registrationBtn.click();
+
+        const passwordConfirmation = page.locator('#signupRepeatPassword')
 
 
         await passwordConfirmation.fill('111')
         await passwordConfirmation.blur()
-        await page.waitForTimeout(100);
-        const borderColor2 = await passwordConfirmation.evaluate((element) => {
-            return window.getComputedStyle(element).borderColor;
-        })
-        await expect(borderColor2).toBe('rgb(220, 53, 69)')
+        await expect(passwordConfirmation).toHaveCSS('border-color', 'rgb(220, 53, 69)')
         await passwordConfirmation.clear()
 
-
     });
+
 
     test('Register button validation', async () => {
         const loginBtn = page.locator('button', {
